789b6ebe882d6f854e2d9f49429d5959e909cf3f
Martin Chaplin <martin.chaplin44@gmail.com>
2019-12-09 21:36:07 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,3 @@
-def greeting = "Hello World" # Define a local variable called 'greeting',
+def greeting = "Hello World"
+  end# Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
